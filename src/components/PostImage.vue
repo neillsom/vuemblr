8989b7e654fcd1f09e94d7cedb7cdd99ab1aeecb
Neill Somerville <neill.somerville@gmail.com>
2020-11-17 23:02:00 -0700

    post cmp refactoring

--- src/components/PostImage.vue
+++ src/components/PostImage.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-   
-    <img
-      v-for="imageUrl in post.content.imageUrl"
-      :key="imageUrl"
-      :alt="`a post from ${post.blogName}`"
-      class="post-image"
-      :src="imageUrl"
-      srcset=""
-      sizes="(max-width: 540px) 100vw, 540px"
-    />
+    <div class="image-container" @click="imageViewer">
+      <div>
+        <a :href="post.content.imageUrl" target="_blank">
+          <img
+            v-for="imageUrl in post.content.imageUrl"
+            :key="imageUrl"
+            :alt="`a post from ${post.blogName}`"
+            class="post-image"
+            :src="imageUrl"
+            srcset=""
+            sizes="(max-width: 540px) 100vw, 540px"
+          />
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,8 +23,18 @@ export default {
   props: {
     post: Object,
   },
+  methods: {
+    imageViewer() {
+      console.log("imageViewer clicked");
+    },
+  },
 };
 </script>
 
 <style>
+#hello {
+}
+.image-container .post-image {
+  cursor: pointer;
+}
 </style>
